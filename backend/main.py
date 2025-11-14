f0bbd00009a428cc38080117069911aa73872b92
SAK <SAK@gmail.com>
2025-11-14 00:08:30 -0500

    Deployment issues fixed

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,10 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["http://localhost:5173",
+        "http://localhost:3000",
+        "https://spotnere.vercel.app",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
